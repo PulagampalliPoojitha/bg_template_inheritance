c09ffce6bdc4f53d98a912b1da8931ba99dc555d
poojitha <ppulagampalli@gmail.com>
2023-06-16 23:05:33 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mdb_jquery(request):
+    return render(request,'mdb_jquery.html')
+def bg(request):
+    return render(request,'bg.html')
